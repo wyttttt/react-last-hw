b161232c0f299f4507e414962abfd99eb15bf653
王宇婷 <44824987+wyttttt@users.noreply.github.com>
2020-11-10 19:36:41 +0800

    Signed-off-by: 王宇婷 <44824987+wyttttt@users.noreply.github.com>

---
+++ build/precache-manifest.7a962eb0cc9a5670d804f8819f11b894.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57ce5f1e2cebe5e8c4a3560d5955ea0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f499bd37fe20891dad34",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "cfd818b6622999dd9f61",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f499bd37fe20891dad34",
+    "url": "/static/js/2.e02c4636.chunk.js"
+  },
+  {
+    "revision": "9f81f95fdf77198dcf92d14b566e110e",
+    "url": "/static/js/2.e02c4636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd818b6622999dd9f61",
+    "url": "/static/js/main.d1ee0fae.chunk.js"
+  },
+  {
+    "revision": "7329818627515a18dae5",
+    "url": "/static/js/runtime-main.581274c4.js"
+  },
+  {
+    "revision": "1dc9ab223bf79eccd83e60a63f82fb0f",
+    "url": "/static/media/caidao.1dc9ab22.png"
+  }
+]);
